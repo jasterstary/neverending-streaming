436177b2147ec014906be8bf2014cf195093584a
jasterstary <web@andybezak.com>
2017-10-31 01:43:29 +0100

    Update neverending-streaming.js

--- neverending-streaming.js
+++ neverending-streaming.js
@@ -1,4 +1,16 @@
-(function(window, $) {
+/*!
+ * AjaxNeverendingStreaming
+ * Version 1.0.0-2017.10.31
+ * Requires javascript :)
+ *
+ * Examples at: https://github.com/jasterstary/neverending-streaming/example
+ * Copyright (c) 2017 JašterStarý
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+;(function(window, $) {
   var document = window.document;
 
   if ( typeof window.CustomEvent !== "function" ) {
